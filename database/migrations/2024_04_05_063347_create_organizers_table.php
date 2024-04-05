0931815f26ff87970dbb249f3816c7d59e0d8d63
fzrilsh <fazriloke18@gmail.com>
2024-04-05 14:48:56 +0700

    Relation organizer with user

--- database/migrations/2024_04_05_063347_create_organizers_table.php
+++ database/migrations/2024_04_05_063347_create_organizers_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('organizers', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users');
             $table->string('email');
             $table->string('name');
             $table->string('telephone');
